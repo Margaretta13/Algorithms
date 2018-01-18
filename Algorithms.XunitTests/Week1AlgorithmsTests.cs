298631d5cbe7542d589c2e410e4fdd84eb0efd6b
lxb768 <lalita.bhetwal@sherwin.com>
2018-01-17 23:06:08 -0500

    Added xUnit tests project because nunit tests are throwing inconclusive test error

---
+++ Algorithms.XunitTests/Week1AlgorithmsTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Algorithms.XunitTests
+{
+    public class Week1AlgorithmsTests
+    {
+        [Theory]
+        [InlineData(3, 5)]
+        [InlineData(1, 9)]
+        [InlineData(9, 9)]
+        public void AddTwoNumbers_Adds_TwoNumbers_xunit(int a, int b)
+        {
+            var alg1 = new Week1Algorithms();
+            int result = alg1.AddTwoNumbers(a, b);
+            Assert.Equal(a + b, result);
+        }
+      
+    }
+}
