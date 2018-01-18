496ea582c543373f366336b6504c7c0cf97f7ad4
lxb768 <lalita.bhetwal@sherwin.com>
2018-01-17 22:02:45 -0500

    Added test project

---
+++ Algorithms/Week1Algorithms.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    public class Week1Algorithms
+    {
+        public int AddTwoNumbers(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
