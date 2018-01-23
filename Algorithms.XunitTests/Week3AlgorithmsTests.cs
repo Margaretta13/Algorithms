7b55369545b1092c46a86bb018d9cabb17b8993a
lxb768 <lalita.bhetwal@sherwin.com>
2018-01-22 22:41:35 -0500

    Got greedy Algorithms first test passing

---
+++ Algorithms.XunitTests/Week3AlgorithmsTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Algorithms.XunitTests
+{
+    public class Week3AlgorithmsTests
+    {
+        private Week3Algorithms week3Algorithms;
+
+        public Week3AlgorithmsTests()
+        {
+            week3Algorithms = new Week3Algorithms();
+        }
+
+        [Theory]
+        [InlineData(2, 2)]
+        [InlineData(28, 6)]
+        public void GetMinimumNumberOfCoins_Gets_MinimumNumberOfCoins(int inputValue, int expectedNumberOfCoins)
+        {
+            int result = week3Algorithms.GetMinimumNumberOfCoins(inputValue);
+
+            Assert.Equal(expectedNumberOfCoins, result);
+        }
+    }
+}
---
+++ Algorithms/Week3Algorithms.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace Algorithms
+{
+    public class Week3Algorithms
+    {
+        private const int One = 1;
+        private const int Five = 5;
+        private const int Ten = 10;
+
+        public int GetMinimumNumberOfCoins(int inputValue)
+        {
+            int numnerOfCoins = Convert.ToInt32(inputValue / Ten);
+            int remainder = inputValue % Ten;
+            if (remainder == 0)
+            {
+                return numnerOfCoins;
+            }
+            
+            numnerOfCoins = numnerOfCoins + Convert.ToInt32(remainder / Five);
+            remainder = remainder % Five;
+            if (remainder == 0)
+            {
+                return numnerOfCoins;
+            }
+
+            numnerOfCoins = numnerOfCoins + Convert.ToInt32(remainder / One);
+            return numnerOfCoins;
+        }
+    }
+}
